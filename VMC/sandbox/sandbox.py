f55d6a41e2d73f69fee8077552941b319b493511
JonahSagers <24sagjon@hawken.edu>
2023-04-27 13:35:29 -0400

    Update sandbox.py

--- VMC/sandbox/sandbox.py
+++ VMC/sandbox/sandbox.py
@@ -1,16 +1,29 @@
-from bell.avr.mqtt.client import MQTTModule
-from bell.avr.mqtt.payloads import AvrFcmVelocityPayload, AvrPcmSetServoOpenClosePayload
-from bell.avr.mqtt.payloads import AvrPcmSetBaseColorPayload
 import time
 
+from bell.avr.mqtt.client import MQTTModule
+from bell.avr.mqtt.payloads import AvrPcmSetBaseColorPayload
 from loguru import logger
+
 
 class Sandbox(MQTTModule):
     def __init__(self) -> None:
         super().__init__()
-        logger.debug("Hello world (init)")
-        self.send_message("avr/pcm/set_base_color",{"wrgb": [0, 100, 0, 0]})
+        logger.debug("Class initialized")
+
+    def hello_world(self) -> None:
+        logger.debug("Hello world")
+
+        payload = AvrPcmSetBaseColorPayload(wrgb=(0, 100, 0, 0))
+        self.send_message("avr/pcm/set_base_color", payload)
+
         logger.debug("Light changed")
+
+
 if __name__ == "__main__":
     box = Sandbox()
-    box.run()
+    box.run_non_blocking()
+
+    box.hello_world()
+
+    while True:
+        time.sleep(0.1)
