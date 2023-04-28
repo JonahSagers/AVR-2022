08fa95e529063302e05908cac02be9864ed502c0
JonahSagers <24sagjon@hawken.edu>
2023-04-28 15:10:56 -0400

    Update sandbox.py

--- VMC/sandbox/sandbox.py
+++ VMC/sandbox/sandbox.py
@@ -10,24 +10,13 @@ class Sandbox(MQTTModule):
         super().__init__()
         logger.debug("Class initialized")
 
-    def hello_world(self) -> None:
-        logger.debug("Hello world")
-
-        payload = AvrPcmSetBaseColorPayload(wrgb=(255, 255, 0, 0))
-        box.send_message("avr/pcm/set_base_color", payload)
-
-        logger.debug("Light changed")
-
-
 if __name__ == "__main__":
     box = Sandbox()
     box.run_non_blocking()
-    #box.hello_world()
     placeholder = 0
     while True:
         placeholder += 0.1
-        saturation = 127.5 + (math.sin(placeholder) * 127.5)
-        #payload = AvrPcmSetBaseColorPayload(wrgb=(0, 0, int(saturation), 0))
+        saturation = 100 + (math.sin(placeholder) * 100)
         box.send_message("avr/pcm/set_base_color", {"wrgb": [0, 0, int(saturation), 0]})
         logger.debug(saturation)
         time.sleep(0.025)
