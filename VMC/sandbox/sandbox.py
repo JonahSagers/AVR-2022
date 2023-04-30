508cf610ed37e612a25bf2c5111978c2b7437aaa
JonahSagers <24sagjon@hawken.edu>
2023-04-30 14:41:27 -0400

    Update sandbox.py

--- VMC/sandbox/sandbox.py
+++ VMC/sandbox/sandbox.py
@@ -16,10 +16,11 @@ if __name__ == "__main__":
     placeholder = 0
     while True:
         placeholder += 0.08
-        saturationR = 50 + (math.sin(placeholder) * 50)
-        saturationG = 50 + (math.sin(placeholder + 1) * 50)
-        saturationB = 50 + (math.sin(placeholder + 2) * 50)
-        box.send_message("avr/pcm/set_base_color", {"wrgb": [0, int(saturationR), int(saturationG), int(saturationB)]})
+        saturationW = 50 + (math.sin(placeholder) * 50)
+        saturationR = 50 + (math.sin(placeholder + 1.57) * 50)
+        saturationG = 50 + (math.sin(placeholder + 3.14) * 50)
+        saturationB = 50 + (math.sin(placeholder + 4.71) * 50)
+        box.send_message("avr/pcm/set_base_color", {"wrgb": [int(saturationW), int(saturationR), int(saturationG), int(saturationB)]})
         #box.send_message("avr/pcm/set_servo_abs", {"servo": 3, "absolute": int(700 + saturationR * 15)})
         logger.debug(saturationR)
         time.sleep(0.0166)
